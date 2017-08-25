7949d935b69805d837ee3cc4c0c5f976a8fdbba8
dzsfo <130066051@qq.com>
2017-08-25 11:56:08 +0800

    更新个人中心跳转到七夕首页 更新微信分享信息

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,11 +5,15 @@ class Index extends Base
 {
     public function index()
     {
-    	$myteam = Db::name('glory_member')->where('uid',$this->uid)->find();
-    	$teaminfo = Db::name('glory_team')->where('id',$myteam['tid'])->find();
-        $team = Db::name('glory_member m')->where('tid',$myteam['tid'])->join("__USER__ u","m.uid=u.id")->field("m.*,u.username,u.phone,u.face")->order("is_captain ASC")->select();
-        $this->assign("teaminfo",$teaminfo);
-        $this->assign("team",$team);
-        return $this->fetch();
+    	if(ismobile()){
+            $this->redirect('/lover');
+        }else{
+	    	$myteam = Db::name('glory_member')->where('uid',$this->uid)->find();
+	    	$teaminfo = Db::name('glory_team')->where('id',$myteam['tid'])->find();
+	        $team = Db::name('glory_member m')->where('tid',$myteam['tid'])->join("__USER__ u","m.uid=u.id")->field("m.*,u.username,u.phone,u.face")->order("is_captain ASC")->select();
+	        $this->assign("teaminfo",$teaminfo);
+	        $this->assign("team",$team);
+	        return $this->fetch();
+	    }
     }
 }
